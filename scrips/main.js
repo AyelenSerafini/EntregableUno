906fb4763258b3fccf4167ccc87c1efa7be08fd0
¨Ayelen <ayelenserafini1996@gmail.com>
2024-07-15 15:55:36 -0300

    EntregableUno

---
+++ scrips/main.js
@@ -0,0 +1,43 @@
+// Variables Constantes//
+
+let nombre 
+let casado 
+let eded
+let apellido
+const url = "www.google.com"
+
+apellido = "Serafini"
+nombre = "Ayelen"
+eded = 28
+casado = true
+
+console.log(nombre)
+console.log(eded)
+console.log(casado)
+console.log(url)
+console.log(nombre + " " + apellido)
+
+// Alert Confirm Prompt //
+
+alert("Bienvenidos")
+
+confirm("¿Eres mayor de edad?")
+
+prompt("¿Cuantos años tienes?")
+
+
+//Array//
+
+const alumnos = ["Sonia", "Pablo", "Juan", "Camila" ]
+const productos = ["Lapiz", "Goma", "Regla","Tiejra"]
+
+const Arraynumero1 = [1,2,3,4]
+
+console.log(Arraynumero1)
+console.log(productos)
+console.log(alumnos)
+
+
+
+
+
